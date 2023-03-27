ef496bd92c9c052e5e48c806177c57c0e060b72b
Tunahanclr <tunaceler@gmail.com>
2023-03-27 17:06:10 +0300

    first commit

---
+++ components/layout/Tuna.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Tuna() {
+  return (
+    <div>Tuna</div>
+  )
+}
---
+++ pages/home/index.jsx
@@ -0,0 +1,9 @@
+import Carousel from "@/components/Carousel";
+import React from "react";
+
+const Index = () => {
+  return <div className=""><Carousel/></div>;
+  
+};
+
+export default Index;

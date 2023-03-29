961cc1cfaecf437d17c22af2aa0307e086f0d2be
Tunahanclr <tunaceler@gmail.com>
2023-03-29 13:07:42 +0300

    ikinci göndermee

--- components/ui/Title.jsx
+++ components/ui/Title.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Title = ({ children, addClass, emin }) => {
-  return <div className={`${addClass}`}>{children}</div>;
+  return <div className={`${addClass} font-dancing`}>{children}</div>;
 };
 
 export default Title;
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,10 +1,14 @@
+import About from "@/components/About";
 import Campaigns from "@/components/Campaigns";
 import Carousel from "@/components/Carousel";
+import MenuWrapper from "@/components/product/MenuWrapper";
 import React from "react";
 
 const Index = () => {
   return <div className=""><Carousel/>
     <Campaigns/>
+    <MenuWrapper/>
+    <About/>
   </div>;
   
 };
